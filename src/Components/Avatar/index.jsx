eb317a6cdb424300178f5beac015f4f24185cb45
Philipe Couto <philipe.couto@zkteco.com>
2023-02-23 09:51:26 -0300

    Ajuste na tela de cadastro e ajuste do botão de adicionar imagem.

--- src/Components/Avatar/index.jsx
+++ src/Components/Avatar/index.jsx
@@ -1,30 +1,49 @@
 import styled from "styled-components";
-import { Button, Card, Row, Col } from 'react-materialize';
+import { Button, Card, Row, Col, Icon } from 'react-materialize';
+
 
 export default function Avatar({ content }) {
-  return<> <AvatarContainer>
-          
-        </AvatarContainer>
-        <FloatButtom 
-                className="red"
-                icon
-                floating
-                large>
-                
-              </FloatButtom>
+  return<>  
+            <AvatarContainer>
+                <AvatarBox>               
+                </AvatarBox>
+                <FloatButtom 
+                    className="red"
+                    icon={<Icon center tiny>add</Icon>}con
+                    floating
+                    node="button"
+                    waves="light"
+                    large>
+                    
+                </FloatButtom>
+            </AvatarContainer>
+           
         </>;
 }
 const FloatButtom = styled(Button)`
-    position: relative;
+    position: absolute;
+    padding-right: 10rem;
+    margin-right: 7rem;
+    border: 5px solid #fff !important;
 
+    @media only screen and (min-width: 360px) {
+        margin-right: 6rem;
+      }
+    
+    @media only screen and (min-width: 500px) {
+        margin-right: 16rem;
+      }
+
+    @media only screen and (min-width: 760px) {
+        margin-right: 4rem;
+      }
 `
 
-const AvatarContainer = styled.div`
+const AvatarBox = styled.div`
     background-color: #44cd6f;
     border-radius: 50%;
     width: 300px;
     height: 300px;
-
     @media only screen and (min-width: 360px) {
         width: 300px;
         height: 300px;
@@ -56,4 +75,24 @@ const AvatarContainer = styled.div`
         width: 300px;
         height: 300px;
       }
+
+`
+
+const AvatarContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: flex-end;
+    align-items: flex-end;
+    flex-wrap: wrap;
+    margin: 10px;
+
+    
+    @media only screen and (min-width: 768px) {
+        width: 300px;
+        height: 300px;
+        margin: 10px;
+    }
+    
 `;
+
